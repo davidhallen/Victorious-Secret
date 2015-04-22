d6439a309210687d3d0b0ce72b7ac19719991780
vicnyd <victor.nyden@gmail.com>
2015-04-22 14:37:23 +0200

    testar lite

--- Test/app/src/main/java/victorious/test/MainActivity.java
+++ Test/app/src/main/java/victorious/test/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    // Githubtest
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
