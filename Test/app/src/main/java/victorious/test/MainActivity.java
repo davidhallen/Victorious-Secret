701d3c06c741820e363bd88e8e9c9d37cc4c01e7
vicnyd <victor.nyden@gmail.com>
2015-04-22 14:38:47 +0200

    githubtest 2

--- Test/app/src/main/java/victorious/test/MainActivity.java
+++ Test/app/src/main/java/victorious/test/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    // Githubtest
+    // Githubtest_2
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
