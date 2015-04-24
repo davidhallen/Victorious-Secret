ee0bf9d196951c8106feb9a61fe3fd6b0df0f62c
Piotri Neto <piotri1991@gmail.com>
2015-04-24 11:25:39 +0200

    TimeLine layout v1

---
+++ Timeline-Alfa/app/src/main/java/bodega/timeline_alfa/MainActivity.java
@@ -0,0 +1,23 @@
+package bodega.timeline_alfa;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        //WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+}
