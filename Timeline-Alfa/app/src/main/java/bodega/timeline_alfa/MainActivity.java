30cd91b442e57436774c97e6cd26c06cd20bfe3b
vicnyd <victor.nyden@gmail.com>
2015-04-24 16:40:19 +0200

    jhj

--- Timeline-Alfa/app/src/main/java/bodega/timeline_alfa/MainActivity.java
+++ Timeline-Alfa/app/src/main/java/bodega/timeline_alfa/MainActivity.java
@@ -5,8 +5,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -19,5 +22,16 @@ public class MainActivity extends ActionBarActivity {
         //WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        TextView question = (TextView) findViewById(R.id.question);
+        Button button1904 = (Button) findViewById(R.id.button1904);
+        button1904.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+
+
+            }
+         });
+
     }
 }
