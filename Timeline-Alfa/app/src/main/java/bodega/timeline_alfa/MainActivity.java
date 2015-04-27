ba12da7321b8c25cdecebbb1d51ab07eea510e74
Piotri Neto <piotri1991@gmail.com>
2015-04-27 10:24:44 +0200

    Fullscreen and removed action bar
    MainActivity.java and activity_main.xml updated

--- Timeline-Alfa/app/src/main/java/bodega/timeline_alfa/MainActivity.java
+++ Timeline-Alfa/app/src/main/java/bodega/timeline_alfa/MainActivity.java
@@ -30,10 +30,9 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-        //WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         init();
